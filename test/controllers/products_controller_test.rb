b528699d24a812c8bc009a4b363d60d260e59210
Daniel US <danielandresus@gmail.com>
2022-12-18 09:52:37 -0300

    flash and validations  added

---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+  test 'render a list of products' do
+    get products_path
+
+    assert_response :success
+    assert_select '.product', 2
+  end
+
+  test 'render a details product page' do
+    get product_path(products(:ps3))
+
+    assert_response :success
+    assert_select '.title', 'PS3 Fat'
+    assert_select '.description', 'PS3 one joystick'
+    assert_select '.price', 'US$ 95'
+  end
+
+  test 'render a new product form' do
+    get new_product_path
+
+    assert_response :success
+    assert_select 'form'
+  end
+
+  test 'allow to create a new product' do
+    post products_path, params: {
+      product: {
+        title: 'Nintendo 64',
+        description: 'Incompleted cables',
+        price: 99
+      }
+    }
+
+    assert_redirected_to products_path
+    assert_equal flash[:notice], 'Product successfully created'
+  end
+
+  test 'does not allow to create a new product with empty fields' do
+    post products_path, params: {
+      product: {
+        title: '',
+        description: 'Incompleted cables',
+        price: 99
+      }
+    }
+
+    assert_response :unprocessable_entity
+  end
+end
